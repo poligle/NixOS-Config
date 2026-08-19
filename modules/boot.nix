a107ffbf8315522faa98b04fe53997bac0885ae3
poligle <poliglesias14@gmail.com>
2026-08-20 00:00:26 +0200

    Deleted quirks in boot.nix

--- modules/boot.nix
+++ modules/boot.nix
@@ -6,9 +6,5 @@
     boot.loader.systemd-boot.configurationLimit = 10;
     boot.supportedFilesystems = [ "exfat" "ext4" "vfat" ];
     services.udisks2.enable = true;
-    services.gvfs.enable = true;
-    boot.kernelParams = [
-        "usb-storage.quirks=05e3:0747:u"
-        "usbcore.quirks=05e3:0747:k"
-    ]; 
+    services.gvfs.enable = true; 
 }
