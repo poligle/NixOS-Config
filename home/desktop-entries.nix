34497278c9da2928adce795a1e3772a331b2508a
poligle <poliglesias14@gmail.com>
2026-08-20 00:16:08 +0200

    Added KiCad Secondary Programs to hidden Desktop Entries

--- home/desktop-entries.nix
+++ home/desktop-entries.nix
@@ -2,48 +2,57 @@
 
 { config, pkgs, lib, ... }:
 let
-  hidden = name: {
-    text = ''
-      [Desktop Entry]
-      Type=Application
-      Name=${name}
-      NoDisplay=true
-    '';
-  };
+    hidden = name:
+    {
+        text = ''
+            [Desktop Entry]
+            Type=Application
+            Name=${name}
+            NoDisplay=true
+        '';
+    };
 in
 {
-  # Hide unwanted launcher entries by overriding them in the user's
-  # applications dir (~/.local/share/applications), which takes precedence.
+    # Hide unwanted launcher entries by overriding them in the user's
+    # applications dir (~/.local/share/applications), which takes precedence.
 
-  xdg.dataFile = {
-    "applications/code-url-handler.desktop" = hidden "Visual Studio Code - URL Handler";
-    "applications/kitty-open.desktop" = hidden "kitty URL Launcher";
-    "applications/org.freedesktop.Xwayland.desktop" = hidden "Xwayland";
-    "applications/xdg-desktop-portal-gtk.desktop" = hidden "Portal";
-    "applications/nixos-manual.desktop" = hidden "NixOS Manual";
-    "applications/uuctl.desktop" = hidden "uuctl";
-    "applications/kitty.desktop" = hidden "kitty";
-    "applications/nvim.desktop" = hidden "Neovim wrapper";
-    "applications/nm-applet.desktop" = hidden "NetworkManager Applet";
-    "applications/nm-connection-editor.desktop" = hidden "Advanced Network Configuration";
-    "applications/blueman-adapters.desktop" = hidden "Bluetooth Adapters";
-    "applications/blueman-manager.desktop" = hidden "Bluetooth Manager";
-    "applications/org.pulseaudio.pavucontrol.desktop" = hidden "Volume Control";
-    "applications/thunar-bulk-rename.desktop" = hidden "Bulk Rename";
-    "applications/thunar-settings.desktop" = hidden "Thunar Preferences";
-    "applications/thunar-volman-settings.desktop" = hidden "Removable Drives and Media";
+    xdg.dataFile =
+    {
+        "applications/code-url-handler.desktop"                 = hidden "Visual Studio Code - URL Handler";
+        "applications/kitty-open.desktop"                       = hidden "kitty URL Launcher";
+        "applications/org.freedesktop.Xwayland.desktop"         = hidden "Xwayland";
+        "applications/xdg-desktop-portal-gtk.desktop"           = hidden "Portal";
+        "applications/nixos-manual.desktop"                     = hidden "NixOS Manual";
+        "applications/uuctl.desktop"                            = hidden "uuctl";
+        "applications/kitty.desktop"                            = hidden "kitty";
+        "applications/nvim.desktop"                             = hidden "Neovim wrapper";
+        "applications/nm-applet.desktop"                        = hidden "NetworkManager Applet";
+        "applications/nm-connection-editor.desktop"             = hidden "Advanced Network Configuration";
+        "applications/blueman-adapters.desktop"                 = hidden "Bluetooth Adapters";
+        "applications/blueman-manager.desktop"                  = hidden "Bluetooth Manager";
+        "applications/org.pulseaudio.pavucontrol.desktop"       = hidden "Volume Control";
+        "applications/thunar-bulk-rename.desktop"               = hidden "Bulk Rename";
+        "applications/thunar-settings.desktop"                  = hidden "Thunar Preferences";
+        "applications/thunar-volman-settings.desktop"           = hidden "Removable Drives and Media";
 
-    # LibreOffice
-    "applications/startcenter.desktop" = hidden "LibreOffice";
-    "applications/draw.desktop" = hidden "LibreOffice Draw";
-    "applications/impress.desktop" = hidden "LibreOffice Impress";
-    "applications/math.desktop" = hidden "LibreOffice Math";
-    "applications/base.desktop" = hidden "LibreOffice Base";
-    "applications/xsltfilter.desktop" = hidden "LibreOffice XSLT based filters";
+        # LibreOffice
+        "applications/startcenter.desktop"                      = hidden "LibreOffice";
+        "applications/draw.desktop"                             = hidden "LibreOffice Draw";
+        "applications/impress.desktop"                          = hidden "LibreOffice Impress";
+        "applications/math.desktop"                             = hidden "LibreOffice Math";
+        "applications/base.desktop"                             = hidden "LibreOffice Base";
+        "applications/xsltfilter.desktop"                       = hidden "LibreOffice XSLT based filters";
 
-    # Qt theming tools (pulled in by Stylix)
-    "applications/kvantummanager.desktop" = hidden "Kvantum Manager";
-    "applications/qt5ct.desktop" = hidden "Qt5 Settings";
-    "applications/qt6ct.desktop" = hidden "Qt6 Settings";
+        # Qt theming tools (pulled in by Stylix)
+        "applications/kvantummanager.desktop"                   = hidden "Kvantum Manager";
+        "applications/qt5ct.desktop"                            = hidden "Qt5 Settings";
+        "applications/qt6ct.desktop"                            = hidden "Qt6 Settings";
+    
+        # KiCad
+        "applications/org.kicad.bitmap2component.desktop"       = hidden "KiCad Bitmap2Component";
+        "applications/org.kicad.eeschema.desktop"               = hidden "KiCad Eeschema";
+        "applications/org.kicad.gerbview.desktop"               = hidden "KiCad Gerbview";
+        "applications/org.kicad.pcbcalculator.desktop"          = hidden "KiCad PCB Calculator";
+        "applications/org.kicad.pcbnew.desktop"                 = hidden "KiCad PCBNew";
   };
 }
