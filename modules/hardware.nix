7b6935137b2dc6aa4d1ad10a8a281ae2bc8079f3
poligle <poliglesias14@gmail.com>
2026-08-21 17:39:09 +0200

    Bluetooth toggled off when power on

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -7,7 +7,7 @@
     security.pam.services.hyprlock.fprintAuth = true;
     zramSwap.enable = true;
     hardware.graphics.enable = true;
-
+    
     # Allow LED change (micmute)
     services.udev.extraRules = ''
         ACTION=="add", SUBSYSTEM=="leds", KERNEL=="platform::micmute", RUN+="${pkgs.coreutils}/bin/chgrp input /sys/class/leds/%k/brightness", RUN+="${pkgs.coreutils}/bin/chmod g+w /sys/class/leds/%k/brightness"
--- modules/network.nix
+++ modules/network.nix
@@ -4,6 +4,21 @@
 {
     networking.networkmanager.enable = true;
     hardware.bluetooth.enable = true;
+    hardware.bluetooth.powerOnBoot = false;
     services.blueman.enable = true;
     networking.networkmanager.wifi.powersave = false;
+
+    systemd.services.bluetooth-off-at-boot = 
+    {
+        description = "Power off Bluetooth adapter at boot";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "bluetooth.service" ];
+        requires = [ "bluetooth.service" ];
+        serviceConfig = 
+        {
+            Type = "oneshot";
+            ExecStartPre = "${pkgs.coreutils}/bin/sleep 3";
+            ExecStart = "${pkgs.bluez}/bin/bluetoothctl power off";
+        };
+    };
 }
