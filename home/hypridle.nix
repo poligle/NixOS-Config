4f9d99f3b7cae602ff0484b6ac7ad457f46df95b
poligle <poliglesias14@gmail.com>
2026-08-25 14:19:05 +0200

    Added DPMS OFF in Hypridle (Bc OLED Monitor) + Fixed times

--- home/hypridle.nix
+++ home/hypridle.nix
@@ -1,21 +1,21 @@
 # hypridle.nix by poligle
 
-{ pkgs, ... }: 
+{ pkgs, ... }:
 {
-	services.hypridle = 
+	services.hypridle =
 	{
 		enable = true;
     
-		settings = 
+		settings =
 		{
-			general = 
+			general =
 			{
 				lock_cmd = "pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
 				before_sleep_cmd = "loginctl lock-session";
 				after_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
 			};
 
-			listener = 
+			listener =
 			[
 				{
 					timeout = 150;
@@ -23,11 +23,16 @@
 					on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -r";
 				}
 				{
-					timeout = 300;
+					timeout = 240;
 					on-timeout = "loginctl lock-session";
 				}
+                {
+                    timeout = 270;
+                    on-timeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off";
+                    on-resume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+                }
 				{
-					timeout = 1800;
+					timeout = 900;
 					on-timeout = "systemctl suspend";
 				}
 			];
